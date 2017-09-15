21c34311f78cd3597cdc1dd2724d86be91f74825
Kyle Daling <kyle-daling@live.com>
2017-09-15 00:19:04 -0700

    Rewrote app to use JavaScript classes
    Also follows a better object oriented approach

--- homepage.js
+++ homepage.js
@@ -1,15 +1,12 @@
 "use strict";
 
 class FuzzySearch {
-    constructor(name) {
-        this.name = name;
+    constructor(item) {
+        this.linkItem = item;
+        this.name = this.linkItem.name;
         this.results = [];
-        this.elm = document.createElement('span');
-        this.inner = document.createElement('span');
-        this.elm.appendChild(this.inner);
     }
     collapseResults(results, termLen) {
-        console.log(results);
         if (results.length == termLen) {
             let resultIDX = 0;
             let tempLen = results.length;
@@ -50,216 +47,196 @@ class FuzzySearch {
         }
         return this.collapseResults(tempResults, termLen);
     }
-    textSnip(start, end) {
-        return document.createTextNode(this.name.substring(start, end));
+    element() {
+        return this.linkItem.element(this.results);
     }
-    get element() {
-        let outer = document.createElement('span');
-        let nameIDX = 0;
-        for (let i = 0; i < this.results.length; i++) {
-            let { start, end } = this.results[i];
-
-            outer.appendChild(this.textSnip(nameIDX, start));
-
-            let strong = document.createElement('strong');
-            strong.appendChild(this.textSnip(start, end + 1));
-            outer.appendChild(strong);
-
-            nameIDX = end + 1;
-        }
-        if (nameIDX < this.name.length)
-            outer.appendChild(this.textSnip(nameIDX, this.name.length));
-        this.elm.replaceChild(outer, this.inner);
-        this.inner = outer;
-        return this.elm;
+    get url() {
+        return this.linkItem.url;
     }
 }
-let a = new FuzzySearch('this is a test');
 
 class LinkItem {
     constructor(data) {
-        this.name = new FuzzySearch(name);
+        this.name = data.name;
         this.url = data.url;
+        this.search = new FuzzySearch(data.name);
     }
-    get element() {
-        let link = document.createElement('a');
-        link.appendChild(document.createTextNode(this.name));
-        link.href = this.url;
-        return link;
+    element(results) {
+        let text = document.createDocumentFragment();
+        if (results) {
+            let nameIDX = 0;
+            for (let i = 0; i < results.length; i++) {
+                let { start, end } = results[i];
+
+                text.appendChild(this.textSnip(nameIDX, start));
+
+                let strong = document.createElement('strong');
+                strong.appendChild(this.textSnip(start, end + 1));
+                text.appendChild(strong);
+
+                nameIDX = end + 1;
+            }
+            if (nameIDX < this.name.length)
+                text.appendChild(this.textSnip(nameIDX, this.name.length));
+        } else {
+            text.appendChild(document.createTextNode(this.name));
+        }
+
+        let elm;
+        if (this.url) {
+            elm = document.createElement('a');
+            elm.href = this.url;
+            elm.appendChild(text);
+        }
+        else {
+            elm = text;
+        }
+        return elm;
+    }
+    textSnip(start, end) {
+        return document.createTextNode(this.name.substring(start, end));
+    }
+}
+class LinkHeader {
+    constructor(data) {
+        this.content = new LinkItem(data);
+    }
+    element() {
+        let header = document.createElement('header');
+        let h1 = document.createElement('h1');
+        h1.appendChild(this.content.element());
+        header.appendChild(h1);
+        return header;
+    }
+    get name() {
+        return this.content.name;
     }
 }
 
 class LinkGroup {
     constructor(group) {
-        this.title = group.title;
+        this.header = new LinkHeader(group.title);
         this.links = group.links.map(item => new LinkItem(item));
+        this.links.sort((a,b) => {
+            let aLower = a.name.toLowerCase();
+            let bLower = b.name.toLowerCase();
+            if (aLower < bLower)
+                return -1;
+            else if (aLower == bLower)
+                return 0;
+            else
+                return 1;
+        });
+    }
+    element() {
+        let section = document.createElement('section');
+        section.appendChild(this.header.element());
+
+        let linkContent = document.createElement('ul');
+        this.links.map(link => {
+            let li = document.createElement('li');
+            li.appendChild(link.element());
+            linkContent.appendChild(li);
+        });
+        section.appendChild(linkContent);
+
+        return section;
+    }
+    get items() {
+        return this.links;
     }
 }
 
-class Results {
-    constructor(items) {
-        this.items = items;
+
+class LinkSearch {
+    constructor(searchItems) {
+        this.wrapper = document.createElement('section');
+        this.wrapper.classList.add('input');
+
+        let form = document.createElement('form');
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+            if (this.results.length > 0 && this.input.value.length > 0) {
+                window.location = this.results[0].url;
+            }
+        });
+        this.input = this.createInput();
+        form.appendChild(this.input);
+        this.wrapper.appendChild(form);
+
+        this.suggestions = document.createElement('div');
+        this.suggestions.id = 'suggestions';
+        this.wrapper.appendChild(this.suggestions);
+        this.searchArray = searchItems.map(item => new FuzzySearch(item));
+        this.results = [];
+
+        window.addEventListener('keydown', e => {
+            if (e.target != this.input) {
+                this.input.focus();
+            }
+        });
+    }
+    element() {
+        return this.wrapper;
+    }
+    createInput() {
+        let input = document.createElement('input');
+        input.type = 'text';
+        input.name = 'q';
+        input.autocomplete = 'off';
+        input.addEventListener('focus', e => e.target.select());
+        input.addEventListener('input', e => this.search(this.input.value));
+        input.focus();
+        return input;
+    }
+    search(term) {
+        if (this.input.value.length > 0) {
+            this.results = this.searchArray.filter(item => item.search(term));
+            this.results.push(new LinkItem({name: `Search: ${term}`, url: `https://www.google.com/search?q=${term}`}));
+            let list = document.createElement('ul');
+            this.results.map(item => {
+                let li = document.createElement('li');
+                li.appendChild(item.element());
+                list.appendChild(li);
+            });
+            let fresh_suggestions = document.createElement('div');
+            fresh_suggestions.id = 'suggestions';
+            fresh_suggestions.appendChild(list);
+            this.wrapper.replaceChild(fresh_suggestions, this.suggestions);
+            this.suggestions = fresh_suggestions;
+        } else {
+            suggestions.style.display = 'none';
+        }
     }
 }
 
 class LinkDisplay {
     constructor(links) {
-        this.displayed = links.display.map(group => new LinkGroup(group));
-        this.hidden = links.hidden;
+        let displayed = links.display.map(group => new LinkGroup(group));
+        let hidden = links.hidden.map(hiddenLink => new LinkItem(hiddenLink));
+        let allItems = displayed.reduce((acc, group) => acc.concat(group.links), []).concat(hidden);
+        this.title = document.createElement('title');
+        this.main = document.createElement('main');
+        this.search = new LinkSearch(allItems);
+
+        let header = new LinkHeader(links.title);
+
+        this.main.appendChild(this.search.element());
+        this.main.appendChild(header.element());
+
+        let linkDiv = document.createElement('div');
+        linkDiv.id = 'links';
+        displayed.map(group => linkDiv.appendChild(group.element()));
+        this.main.appendChild(linkDiv);
+
+        this.title.appendChild(document.createTextNode(header.name));
+        document.head.appendChild(this.title);
+
+        document.body.appendChild(this.main);
     }
 }
 
 fetch('links.json').then(data => data.json()).then(data => {
-    console.log(new LinkDisplay(data));
+    new LinkDisplay(data);
 });
 
-var getURL = function(url) {
-    return new Promise(function (resolve, reject) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open('GET', url);
-        xhttp.send();
-        xhttp.onload = function () {
-            if (this.status >= 200 && this.status < 300) {
-                resolve(this.response);
-            } else {
-                reject(this.statusText);
-            }
-        }
-        xhttp.onerror = function() {
-            reject(this.statusText);
-        }
-    });
-}
-var render = {
-    core: {
-        main: document.getElementById("main"),
-        links: document.getElementById("links"),
-        q: document.getElementById("q"),
-        suggestions: document.getElementById("suggestions"),
-        startTime: new Date(),
-        createHeader: function(tobj) {
-            let header = document.createElement("header");
-            let h1 = document.createElement("h1");
-            let a = document.createElement("a");
-            a.innerHTML = tobj['name'] ? tobj['name'] : "Undefined";
-            document.title = tobj['name'];
-            a.href = tobj['url'];
-            h1.appendChild(a);
-            header.appendChild(h1);
-            this.main.appendChild(header);
-        },
-        linkDisplay:function(dobj) {
-            this.links = document.createElement("div");
-            for (let i = 0; i < dobj.length; i++) {
-                let o = dobj[i];
-                let section = document.createElement("section");
-                if (o['title']) {
-                    let header = document.createElement("header");
-                    let h1 = document.createElement("h1");
-                    if (o['title']['url']) {
-                        h1.appendChild(this.createLink(o['title']));
-                    } else {
-                        h1.innerHTML = o['title']['name'];
-                    }
-                    header.appendChild(h1);
-                    section.appendChild(header);
-                }
-                if (o['links']) {
-                    let ul = document.createElement('ul');
-                    o['links'].sort(function (a,b) {
-                        if (a['name'] < b['name']) {
-                            return -1;
-                        } else if (a['name'] > b['name']) {
-                            return 1;
-                        } else {
-                            return 0;
-                        }
-                    });
-                    for (let j = 0; j < o['links'].length; j++) {
-                        let li = document.createElement('li');
-                        li.appendChild(this.createLink(o['links'][j]));
-                        ul.appendChild(li);
-                    }
-                    section.appendChild(ul);
-                }
-                this.links.appendChild(section);
-            }
-            this.main.appendChild(this.links);
-        },
-        createLink: function (linkObject) {
-            let a = document.createElement('a');
-            a.innerHTML = linkObject['name'];
-            a.href = linkObject['url'];
-            return a;
-        },
-        inputSearch: function (data) {
-            q.addEventListener("keyup", function (e) {
-                if (e.key == "Enter") {
-                    e.preventDefault();
-                    let firstSuggestion = suggestions.getElementsByTagName("a");
-                    if (q.value.length > 0 && firstSuggestion) {
-                        window.location = firstSuggestion[0].href;
-                    }
-                } else {
-                    if (q.value.length == 0) {
-                        suggestions.style.display = "none";
-                    } else {
-                        suggestions.style.display = "block";
-                        while (suggestions.lastChild) {
-                            suggestions.removeChild(suggestions.lastChild);
-                        }
-                        let ul = document.createElement("ul");
-                        let total = 0;
-                        for (let i = 0; i < data.length && total < 5; i++) {
-                            if (fuzzy_match_simple(q.value, data[i]['name'])) {
-                                total++;
-                                let li = document.createElement("li");
-                                let a = document.createElement("a");
-                                a.innerText = data[i]['name'];
-                                a.href = data[i]['url'];
-                                li.appendChild(a);
-                                ul.appendChild(li);
-                            }
-                        }
-                        if (total == 0) {
-                            let li = document.createElement("li");
-                            let a = document.createElement("a");
-                            a.innerText = `Search: ${q.value}`;
-                            a.href = `https://google.com/search?q=${q.value}`;
-                            li.appendChild(a);
-                            ul.appendChild(li);
-                        }
-                        suggestions.appendChild(ul);
-                    }
-                }
-            }, false);
-        }
-    },
-    all:function (data) {
-        let odata = JSON.parse(data);
-        this.core.createHeader(odata['title']);
-        this.core.linkDisplay(odata['display']);
-        let combined = odata['hidden'];
-        for (let i = 0; i < odata['display'].length; i++) {
-            combined = combined.concat(odata['display'][i]['links']);
-        }
-        this.core.inputSearch(combined);
-    }
-}
-// var j = getURL("links.json").then(function (data) {render.all(data)});
-function fuzzy_match_simple(pattern, str) {
-    var patternIdx = 0;
-    var strIdx = 0;
-    var patternLength = pattern.length;
-    var strLength = str.length;
-
-    while (patternIdx != patternLength && strIdx != strLength) {
-        var patternChar = pattern.charAt(patternIdx).toLowerCase();
-        var strChar = str.charAt(strIdx).toLowerCase();
-        if (patternChar == strChar)
-            ++patternIdx;
-        ++strIdx;
-    }
-
-    return patternLength != 0 && strLength != 0 && patternIdx == patternLength ? true : false;
-}
