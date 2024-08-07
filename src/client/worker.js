15da3d62706d2ba284c8a8ed9f784e20503859aa
Kyle Daling <kyle-daling@live.com>
2024-08-06 21:30:41 -0600

    Fix excessive caching

--- src/client/worker.js
+++ src/client/worker.js
@@ -1,27 +1,35 @@
 // Service worker for caching resources
 
+const CACHE_NAME = "homepage-cache";
 
-const CACHE_NAME = 'homepage-cache';
+const URLS_TO_CACHE = [
+    "./",
+    "index.html",
+    "homepage.js",
+    "homepage.css",
+    "links.json",
+];
 
-const URLS_TO_CACHE = ['./', 'index.html', 'homepage.js', 'homepage.css', 'links.json'];
-
-self.addEventListener('install', function install (e) {
-    e.waitUntil(caches.open(CACHE_NAME).then(
-        function(cache) {
+self.addEventListener("install", function install(e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
             return cache.addAll(URLS_TO_CACHE);
         })
     );
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith(caches.open(CACHE_NAME).then(cache => {
-        return cache.match(e.request).then(response => {
-            let fetchPromise = fetch(e.request).then(networkResponse => {
-                cache.put(e.request, networkResponse.clone());
-                return networkResponse;
+self.addEventListener("fetch", function (e) {
+    e.respondWith(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.match(e.request).then((cacheResponse) => {
+                let fetchPromise = fetch(e.request).then((networkResponse) => {
+                    if (cacheResponse) {
+                        cache.put(e.request, networkResponse.clone());
+                    }
+                    return networkResponse;
+                });
+                return cacheResponse || fetchPromise;
             });
-            return response || fetchPromise;
-        });
-    }));
+        })
+    );
 });
-
